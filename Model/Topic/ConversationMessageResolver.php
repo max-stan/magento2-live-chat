a3fb9afe169d61326d7706ea6b566f3947ea4494
Max Stan <stanislavskiymaksym@gmail.com>
2026-02-15 20:24:20 +0200

    Fixed message resolver class name

--- Model/Topic/ConversationMessageResolver.php
+++ Model/Topic/ConversationMessageResolver.php
@@ -6,9 +6,9 @@ namespace MaxStan\LiveChat\Model\Topic;
 use MaxStan\LiveChat\Api\Data\ConversationInterface;
 use MaxStan\LiveChat\Model\ResourceModel\Conversation\Collection;
 use MaxStan\LiveChat\Model\ResourceModel\Conversation\CollectionFactory;
-use MaxStan\Mercure\Api\TopicsResolver;
+use MaxStan\Mercure\Api\TopicsResolverInterface;
 
-readonly class ConversationMessageResolver implements TopicsResolver
+readonly class ConversationMessageResolver implements TopicsResolverInterface
 {
     public const string IRI = 'conversation_index_index';
 
