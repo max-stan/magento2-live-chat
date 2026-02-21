81242ee711ab27266947c1c828cf41e8a2a7162d
Max Stan <stanislavskiymaksym@gmail.com>
2026-02-22 00:20:10 +0200

    Added echo statmeent

--- .github/workflows/pre_project_script.sh
+++ .github/workflows/pre_project_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Configure Hyva repo"
 composer config repositories.private-packagist composer https://hyva-themes.repo.packagist.com/m248-test-367mmnuc7j/
 composer config --global --auth http-basic.hyva-themes.repo.packagist.com token $HYVA_PACKAGIST_TOKEN
